53c69cabb7129c98a528ddf2931239f95771943c
Rocco Antonio Civitillo <roccoantoniocivitillo@MacBooks-Air.homenet.telecomitalia.it>
2018-06-18 10:48:24 +0200

    mod

--- birre.js
+++ birre.js
@@ -17,7 +17,7 @@ var birre = [{
     descrizione: "Birra normale chiara di bassa fermentazione, Premium Lager. Prodotta con malto, mais, luppolo, acqua.Le migliori materie prime, una lunga maturazione e la leggerissima acqua delle Prealpi biellesi conferiscono, sin dal 1846, un gusto pieno e raffinato.Birra assai equilibrata che evidenzia un notevole sentore di floreale e di fruttato grazie ai lieviti speziati usati per la sua produzione.",
     tipo: ['lager', 'bionda'],
     gradoAlcolico: '4.8%'
-} , {
+}, {
     id: 4,
     nome: 'Duvel',
     descrizione: "La Duvel è una birra chiara di fermentazione alta trattata dall'industria della birra Duvel Moortgat in Belgio. Rappresenta l'85% della produzione di questa industria, che produce anche la birra Maredsous. È una birra in stile Golden Strong Ale, con l'8,5% di alcool (vol). L'etichetta raccomanda di servirla ad una temperatura tra 6 e 10 °C (anche se alcuni con le birre di fermentazione alta piuttosto la servono a temperatura di cantina, tra 12 e 14 °C). Produce una schiuma particolarmente ricca, e così l'industria della birra ha preparato un bicchiere speciale per metterla in risalto.",
@@ -29,7 +29,7 @@ var birre = [{
     descrizione: "Nel 1859 nasceva in Italia una birra genuina: Birra Moretti Ricetta Originale. Da quella data il processo produttivo è rimasto del tutto invariato per restituirci integro il sapore di un’epoca. Birra Moretti Ricetta Originale è una birra a bassa fermentazione, ottenuta dalla miscela di luppoli pregiati, che le danno un gradevole sapore finemente amaro e bilanciano, con note floreali, gli iniziali profumi di malto d’orzo. Da consumare preferibilmente in buona compagnia",
     tipo: ['bock', 'rossa'],
     gradoAlcolico: '7.2%'
-} , {
+}, {
     id: 6,
     nome: 'Guinness',
     descrizione: "La Guinness è probabilmente una delle birre stout (ovvero scure e forti) più famose e amate in tutto il mondo, dall’aspetto e sapore inconfondibili. La sua storia leggendaria legata all’Irlanda e a Dublino, insieme al suo modo unico di comunicare, fanno della Guinness un mito assoluto veicolato in ogni angolo del pianeta.",
