f8d3495fe8e3280525c405e293831de6ea7c0462
Irina <irina.chirkunova86@gmail.com>
2018-06-18 12:26:41 +0200

    creata main.js e birre.html

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+/*<div class="col-md-6">
+    <div class="row">
+        <div class="col-md-4"><img></div>
+            <div class="col-md-8"><h3></h3>
+                <p></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button type="button" class="btn btn-secondary">Aggiungi al carello</button>
+            </div>
+        </div>
+    </div>*/
